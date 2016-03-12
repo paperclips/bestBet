69db61687d8ba36f9732a38cc94a60d3fefbf804
pavelmp <pavelmp@gmail.com>
2016-03-11 20:20:40 -0800

    Implement database schemas using sequelize and postgres

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,15 @@
 // db configuration
+var Sequelize = require('sequelize');
+
+var LOCAL_DB = {
+  name: 'paperclipsdb',
+  username: 'postgres',
+  password: '',
+  options: { dialect: 'postgres', logging: false }
+};
+
+var db = new Sequelize(LOCAL_DB.name, LOCAL_DB.username, LOCAL_DB.password, LOCAL_DB.options);
+
+db.sync(); //Create database and tables if they don't exist
+
+module.exports = db;
