eb8c99723636085b12bbbe8890dae47409b4d1fe
pavelmp <pavelmp@gmail.com>
2016-03-17 20:33:22 -0700

    Fix package json file

--- client/index.ios.js
+++ client/index.ios.js
@@ -4,18 +4,18 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import immutable from 'immutable';
 
-//import rootReducer from './src/components/reducers';
-//import Router from './src/components/router';
+// import rootReducer from './src/components/reducers';
+// import Router from './src/components/router';
 
 import App from './App.js';
 
-//create a store that has redux-thunk middleware enabled
-//const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+// create a store that has redux-thunk middleware enabled
+// const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
-//init store with rootReducer
-//const store = createStoreWithMiddleware(rootReducer);
+// init store with rootReducer
+// const store = createStoreWithMiddleware(rootReducer);
 
-//console.log(store.getState().toJS());
+// console.log(store.getState().toJS());
 
 class client extends Component {
   render() {
@@ -28,5 +28,5 @@ class client extends Component {
   }
 }
 
-//export default store;
+// export default store;
 AppRegistry.registerComponent('client', () => client);
