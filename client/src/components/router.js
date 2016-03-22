a0a763dccdcfed9bdd9acd9b7268f0d16642dbb9
Jackie <77.Jackie@gmail.com>
2016-03-22 15:39:25 -0700

    on login, user redirects to mapview

--- client/src/components/router.js
+++ client/src/components/router.js
@@ -8,10 +8,10 @@ import Login from '../containers/container_login';
 // import Signup from '../containers/container_signup';
 // import Traits from '../containers/container_trait';
 // import Vote from '../containers/container_vote';
-// import Map from '../containers/container_map';
+import Map from '../containers/container_map';
 // import Menu from '../containers/container_menu';
 
-const ROUTES = { Login };
+const ROUTES = { Login, Map };
 
 export default class Router extends Component {
   renderScene(route, navigator) {
--- client/src/containers/container_map.js
+++ client/src/containers/container_map.js
@@ -3,10 +3,9 @@ import { connect } from 'react-redux';
 import Map from '../components/map';
 
 import userMoves from '../actions/action_userMoves';
-import setTag from '../actions/action_set_tag';
 
 function mapStateToProps({ user, socket, establishments }) {
   return { user, socket, establishments };
 }
 
-export default connect(mapStateToProps, { userMoves, setTag })(Map);
+export default connect(mapStateToProps, { userMoves })(Map);
