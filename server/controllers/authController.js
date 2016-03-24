9b3f9003609e0a15adc660612c00922747246f27
Brian Ronaghan <brian.ronaghan@gmail.com>
2016-03-23 19:58:11 -0700

    format tweaks

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,16 +4,13 @@ var jwt = require( 'jwt-simple' );
 
 function signup (req, res) {
   var userName = req.body.userName;
-  var traitCombo = req.body.traitCombo;
-  userCtrl.findUser(userName).then(function(oldUser){
-    if(oldUser){
+  userCtrl.findUser(userName).then(function(user){
+    if(user){
       res.status(401).send({error: 'This username is taken'});
     } else {
       userCtrl.addUser(req.body).then(function(user){
         var token = jwt.encode(user.userName, 'secret');
-        console.log(user, 'NEW USER!!!');
-        userCtrl.setUserTraits({userId: user.id, traitCombo: traitCombo});
-        res.status(200).send({id: user.id, name: user.name, userName: user.userName, token: token, traitCombo: traitCombo});  
+        res.status(200).send({id: user.id, name: user.name, userName: user.userName, token: token});  
       })
     }
   })
@@ -22,13 +19,13 @@ function signup (req, res) {
 function login (req, res) {
   var userName = req.body.userName;
   var password = req.body.password;
-  console.log('GOT TO HERE:',req.body);
 
   userCtrl.checkPass(userName, password,function(match,user){
     if(!match){
       res.status(401).send({error:'Incorrect username or password'})
     } else {
       //Get user preferences
+      
       userCtrl.getUserTraits(user.id).then(function(traitRecord){
         var traitCombo = traitRecord.traitCombo;
         var token = jwt.encode(userName, 'secret');
