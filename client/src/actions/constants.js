cff3015f68a1206998887fa7b6c238941e3987ba
pavelmp <pavelmp@gmail.com>
2016-03-29 21:53:38 -0700

    Delete scoreReducer and score action files

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -9,6 +9,4 @@ export const UPDATE_USER_ZONE = 'UPDATE_USER_ZONE';
 export const RESET_TRAITS = 'RESET_TRAITS';
 export const LOGOUT = 'LOGOUT';
 export const CLEAR_SOCKET = 'CLEAR_SOCKET';
-export const REPLACE_SCORES = 'REPLACE_SCORES';
-export const UPDATE_SCORES = 'UPDATE_SCORES';
 
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import estabReducer from './estabReducer';
 import socketReducer from './socketReducer';
-import scoreReducer from './scoreReducer';
 
 module.exports = combineReducers({
   user: userReducer,
   allData: estabReducer,
   socket: socketReducer
-  // scores: scoreReducer
 })
